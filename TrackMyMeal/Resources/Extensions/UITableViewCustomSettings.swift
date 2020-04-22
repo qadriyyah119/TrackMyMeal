a77043d0beee76cbfcbd7a253b7b49532045a5fd
QThomas <qmadinah@gmail.com>
2020-04-22 15:07:16 -0400

    created Resources file for design extensions and Theme file. Also updated font, and colors for main tableView

---
+++ TrackMyMeal/Resources/Extensions/UITableViewCustomSettings.swift
@@ -0,0 +1,21 @@
+//
+//  UITableViewCustomSettings.swift
+//  TrackMyMeal
+//
+//  Created by Qadriyyah Griffin on 4/22/20.
+//  Copyright © 2020 Qadriyyah Thomas. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewCustomSettings: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TrackMyMeal/Resources/Theme.swift
@@ -0,0 +1,9 @@
+//
+//  Theme.swift
+//  TrackMyMeal
+//
+//  Created by Qadriyyah Griffin on 4/22/20.
+//  Copyright © 2020 Qadriyyah Thomas. All rights reserved.
+//
+
+import Foundation
