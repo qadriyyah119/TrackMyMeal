fc25dc03fe042a0fab2fab260b683475d7bf2f5c
QThomas <qmadinah@gmail.com>
2020-08-31 22:00:35 -0400

    In Progress - add animated circle progress bar to dashboard scene.

--- TrackMyMeal/Controllers/DashboardViewController.swift
+++ TrackMyMeal/Controllers/DashboardViewController.swift
@@ -8,26 +8,76 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController, UITableViewDataSource {
-
-  @IBOutlet weak var tableView: UITableView!
+class DashboardViewController: UIViewController {
   
+  @IBOutlet weak var backgroundImageView: UIImageView!
+  
+  let shapeLayer = CAShapeLayer()
+  
+  let percentageLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Start"
+    label.textAlignment = .center
+    label.font = UIFont.boldSystemFont(ofSize: 32)
+    return label
+  }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      backgroundImageView.image = #imageLiteral(resourceName: "dashboard2")
+      
+      view.addSubview(percentageLabel)
+      percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+      percentageLabel.center = view.center
+      
+      let trackLayer = CAShapeLayer()
+      // create circle
+      let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+      
+      trackLayer.path = circularPath.cgPath
+      
+      trackLayer.strokeColor = Theme.accentColor?.cgColor
+      trackLayer.fillColor = Theme.backgroundColor?.cgColor
+      trackLayer.lineWidth = 10
+      trackLayer.lineCap = CAShapeLayerLineCap.round
+      trackLayer.position = view.center
+      view.layer.addSublayer(trackLayer)
+      
+      shapeLayer.path = circularPath.cgPath
+      
+      shapeLayer.strokeColor = Theme.tintColor?.cgColor
+      shapeLayer.fillColor = Theme.backgroundColor?.cgColor
+      shapeLayer.lineWidth = 10
+      shapeLayer.lineCap = CAShapeLayerLineCap.round
+      shapeLayer.position = view.center
+      
+      shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+      
+      shapeLayer.strokeEnd = 0
+      
+      view.layer.addSublayer(shapeLayer)
+      
+      view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
 
-      tableView.dataSource = self
+
     }
+  
+  fileprivate func animateCircle() {
+    let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
     
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    basicAnimation.toValue = 1
+    basicAnimation.duration = 2
+    basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+    basicAnimation.isRemovedOnCompletion = false
+    
+    shapeLayer.add(basicAnimation, forKey: "urSoBasic")
   }
   
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! DashboardCalorieCell
+  @objc private func handleTap() {
+    print("attempting to animate stroke")
     
-    return cell
+    animateCircle()
   }
-    
 
 }
